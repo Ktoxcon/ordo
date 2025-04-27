fe179a9b09b62e7bc1a7af1643ef7ff8cea6a33b
Ktoxcon <kennethtoxro@gmail.com>
2025-04-26 19:34:37 -0600

    chore: removes unused morgan dependency

--- backend/src/interfaces/http/app/index.ts
+++ backend/src/interfaces/http/app/index.ts
@@ -4,13 +4,11 @@ import { TaskRoutes } from "@ordo/interfaces/http/routes/task.routes";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
-import morgan from "morgan";
 
 export const app = express();
 
 app.use(cors());
 app.use(cookieParser());
-app.use(morgan("combined"));
 
 app.use("/api/auth", AuthRoutes);
 app.use("/api/tasks", TaskRoutes);
